ffb2d836f7f077c700df8f5b124af1f4ca7ebb92
Peter Gyorfi <apgyorfi@protonmail.com>
2023-04-28 21:07:11 +0200

    v-bind one object

--- 01-syntax/04-v-bind-with-object/main.js
+++ 01-syntax/04-v-bind-with-object/main.js
@@ -1,12 +1,14 @@
 const app = Vue.createApp({
   setup() {
-    const src =
-      'https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg'
-    const alt = 'Vue.js'
-    const width = 50;
 
+    const logo = {
+      src: 'https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg',
+      alt: 'Vue.js',
+      width: 50,
+
+    }
     return {
-      src, alt, width
+      logo
     }
     
   }
