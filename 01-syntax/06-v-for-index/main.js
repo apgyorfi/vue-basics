b0a77fdf3081240b22765ef83acb05bd88228d0d
Peter Gyorfi <apgyorfi@protonmail.com>
2023-04-28 21:25:34 +0200

    v-for add index

--- 01-syntax/06-v-for-index/main.js
+++ 01-syntax/06-v-for-index/main.js
@@ -2,15 +2,12 @@ const app = Vue.createApp({
   setup() {
     const frameworks = [
       {
-        id: 1,
         name: 'Vue',
       },
       {
-        id: 2,
         name: 'React',
       },
       {
-        id: 3,
         name: 'Angular',
       },
     ]
